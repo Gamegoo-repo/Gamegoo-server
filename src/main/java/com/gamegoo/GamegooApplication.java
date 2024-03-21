c083aeeeb895ec16657131b53fce2113f7be38e0
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-03-21 15:46:07 +0900

    [Init]: 프로젝트 생성

---
+++ src/main/java/com/gamegoo/GamegooApplication.java
@@ -0,0 +1,13 @@
+package com.gamegoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamegooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamegooApplication.class, args);
+	}
+
+}
