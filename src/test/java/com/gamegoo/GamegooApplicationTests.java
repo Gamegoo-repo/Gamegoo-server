272010e4143be7e6bc634096211f7808a29a0938
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-03-21 15:46:07 +0900

    [Init]: 프로젝트 생성

---
+++ src/test/java/com/gamegoo/GamegooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamegoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamegooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
