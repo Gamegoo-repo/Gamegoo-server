ed04bac3b986c597023a9659fbbed80da10dc6a0
Eunjin Kim <jennyeunjin@khu.ac.kr>
2024-03-22 20:50:08 +0900

    :sparkles: [Feat]: 공통 에러 처리 구현

--- src/main/java/com/gamegoo/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/gamegoo/apiPayload/code/status/ErrorStatus.java
@@ -15,9 +15,11 @@ public enum ErrorStatus implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
     _BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
-    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다.");
+    _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
 
+    // 테스트
+    TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트");
 
     private final HttpStatus httpStatus;
     private final String code;
