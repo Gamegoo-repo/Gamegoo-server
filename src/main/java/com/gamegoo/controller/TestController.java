cec0270702624d32d9623b3f1b42af2c2e26e784
Rimi <lora3226@inu.ac.kr>
2024-04-08 21:39:30 +0900

    👷[CD]:GitHub Actions, Docker 배포 관련

--- src/main/java/com/gamegoo/controller/TestController.java
+++ src/main/java/com/gamegoo/controller/TestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
-    @GetMapping("/test/hell1o")
+    @GetMapping("/test/hello")
     @Operation(summary = "swagger 테스트용 API 입니다.", description = "simple API for swagger test!")
     public String hello() {
         return "Swagger Setting Success~~!";
