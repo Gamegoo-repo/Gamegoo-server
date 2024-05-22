b7735349a6ccb1fcaad3fbacd4fe1a4bb34676a0
Rimi <lora3226@inu.ac.kr>
2024-05-22 18:27:01 +0900

    :sparkles: [Feat]: 회원, 게임스타일, 챔피언, 게시판 관련 JPA Entity 작성

---
+++ src/main/java/com/gamegoo/domain/Board.java
@@ -0,0 +1,49 @@
+package com.gamegoo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Board")
+@Getter
+@Setter
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "board_id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(name = "mode", nullable = false)
+    private Integer mode;
+
+    @Column(name = "main_position", nullable = false)
+    private Integer mainPosition;
+
+    @Column(name = "sub_position", nullable = false)
+    private Integer subPosition;
+
+    @Column(name = "want_position", nullable = false)
+    private Integer wantPosition;
+
+    @Column(name = "voice", nullable = false)
+    private Boolean voice;
+
+    @Column(name = "content", nullable = false, length = 3000)
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
+
