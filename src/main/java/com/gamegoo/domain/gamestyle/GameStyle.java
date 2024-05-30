87fde5f13f6879880fc33cc0168c6a9a6956ce7f
hzee97 <janghyeonji0830@naver.com>
2024-05-30 19:57:57 +0900

    :bug: [Fix] : Board.java 충돌 오류 수정

--- src/main/java/com/gamegoo/domain/gamestyle/GameStyle.java
+++ src/main/java/com/gamegoo/domain/gamestyle/GameStyle.java
@@ -1,28 +1,29 @@
 package com.gamegoo.domain.gamestyle;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.gamegoo.domain.Member;
+import com.gamegoo.domain.common.BaseDateTimeEntity;
+import lombok.*;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "GameStyle")
 @Getter
 @Setter
-public class GameStyle {
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class GameStyle extends BaseDateTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "gamestyle_id", nullable = false)
     private Long id;
 
-    @Column(name = "style_name", nullable = false, length = 30)
+    @Column(name = "style_name", nullable = false, length = 100)
     private String styleName;
 
-    @Column(name = "created_at")
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 
 }
