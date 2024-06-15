3d42eee4211c12310842eba213125b07c996ea79
Rimi <lora3226@inu.ac.kr>
2024-06-15 16:03:47 +0900

    :sparkles: [Feat] 회원가입 로직 구현, BaseDateTimeEntity 사용을 위한 EnableJpaAuditing 추가

---
+++ src/main/java/com/gamegoo/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.gamegoo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .antMatchers("/api/join/local").permitAll()
+                        .anyRequest().authenticated());
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/gamegoo/service/JoinService.java
@@ -0,0 +1,44 @@
+package com.gamegoo.service;
+
+import com.gamegoo.domain.Member;
+import com.gamegoo.domain.enums.LoginType;
+import com.gamegoo.dto.member.JoinDTO;
+import com.gamegoo.repository.MemberRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // 생성자
+    public JoinService(MemberRepository memberRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.memberRepository = memberRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    // 회원가입 로직
+
+    public void JoinProcess(JoinDTO joinDTO) {
+
+        // DTO로부터 데이터 받기
+        String email = joinDTO.getEmail();
+        String password = joinDTO.getPassword();
+
+        // 중복 확인은 이메일 인증 코드 발급 API에서 진행
+
+        // DB에 넣을 정보 설정
+        Member member = Member.builder()
+                .email(email)
+                .password(bCryptPasswordEncoder.encode(password))
+                .loginType(LoginType.GENERAL)
+                .profileImage("default")
+                .blind(false)
+                .build();
+
+        // DB에 저장
+        memberRepository.save(member);
+    }
+}
