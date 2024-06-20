8e3d9ed6f07c1b3fce63ab4150304d2661b09b9a
Rimi <lora3226@inu.ac.kr>
2024-06-20 22:06:15 +0900

    :sparkles: [Feat] jpa.hibernate.ddl-auto가 Create일 경우 게임 스타일 생성하기

---
+++ src/main/java/com/gamegoo/repository/gamestyle/GameStyleRepository.java
@@ -0,0 +1,7 @@
+package com.gamegoo.repository.gamestyle;
+
+import com.gamegoo.domain.gamestyle.GameStyle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameStyleRepository extends JpaRepository<GameStyle, Long> {
+}
