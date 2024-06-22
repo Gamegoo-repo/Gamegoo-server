0380dedf8c0794ceea929b67c1b7d3f7f0998e55
Rimi <lora3226@inu.ac.kr>
2024-06-22 13:42:27 +0900

    :sparkles: [Feat] 주/부 포지션 API

--- src/main/java/com/gamegoo/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/com/gamegoo/apiPayload/code/status/ErrorStatus.java
@@ -17,7 +17,6 @@ public enum ErrorStatus implements BaseErrorCode {
     _UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
-
     // 테스트
     TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "테스트"),
 
@@ -31,7 +30,10 @@ public enum ErrorStatus implements BaseErrorCode {
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "JWT400", "유효하지 않은 jwt 토큰입니다."),
 
     // GameStyle 관련 에러
-    GAMESTYLE_NOT_FOUND(HttpStatus.NOT_FOUND, "GAMESTYLE404", "해당 게임 스타일을 찾을 수 없습니다.");
+    GAMESTYLE_NOT_FOUND(HttpStatus.NOT_FOUND, "GAMESTYLE404", "해당 게임 스타일을 찾을 수 없습니다."),
+
+    // Position 관련 에러
+    POSITION_NOT_FOUND(HttpStatus.NOT_FOUND, "POSITION404", "해당 Position을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
