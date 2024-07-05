822d5e99092d9a30b75ab59b03a2bfa68da4cd1f
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-06 00:53:43 +0900

    :sparkles: [Feat]: ReportTypeMapping 엔티티 및 매핑 추가

--- src/main/java/com/gamegoo/domain/report/ReportTypeMapping.java
+++ src/main/java/com/gamegoo/domain/report/ReportTypeMapping.java
@@ -1,4 +1,28 @@
 package com.gamegoo.domain.report;
 
-public class ReportTypeMapping {
+import com.gamegoo.domain.common.BaseDateTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ReportTypeMapping")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReportTypeMapping extends BaseDateTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_type_mapping_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_type_id", nullable = false)
+    private ReportType reportType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "report_id", nullable = false)
+    private Report report;
+
 }
