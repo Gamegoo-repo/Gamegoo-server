22f6dd0d34ce7aa255358d54f4ea7c789ddc2d34
hzee97 <janghyeonji0830@naver.com>
2024-07-06 17:47:08 +0900

    :sparkles: [Feat] 회원 신고 API 구현.

--- src/main/java/com/gamegoo/repository/report/ReportTypeRepository.java
+++ src/main/java/com/gamegoo/repository/report/ReportTypeRepository.java
@@ -3,8 +3,6 @@ package com.gamegoo.repository.report;
 import com.gamegoo.domain.report.ReportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ReportTypeRepository extends JpaRepository<ReportType, Long> {
 
 }
