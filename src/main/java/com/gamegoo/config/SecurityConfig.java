2334b440584ce13da4cc36dc05ae72136d6c1a2f
Rimi <lora3226@inu.ac.kr>
2024-07-06 14:25:14 +0900

    :recycle: [Refactor] @Valid 어노테이션 추가

--- src/main/java/com/gamegoo/config/SecurityConfig.java
+++ src/main/java/com/gamegoo/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
                 .httpBasic(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests((auth) -> auth
-                        .antMatchers("/api/member/join", "/api/member/login", "/api/member/email/**", "/api/member/refresh").permitAll()
+                        .antMatchers("/", "/api/member/join", "/api/member/login", "/api/member/email/**", "/api/member/refresh").permitAll()
                         .antMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(new JWTExceptionHandlerFilter(), UsernamePasswordAuthenticationFilter.class)
