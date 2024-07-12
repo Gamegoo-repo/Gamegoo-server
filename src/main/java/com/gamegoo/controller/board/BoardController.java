9002ddcba66daa50b5be14f225b0998825815834
hzee97 <janghyeonji0830@naver.com>
2024-07-12 22:50:31 +0900

    :memo: [Docs] 게시판 글 작성 API Swagger 명세

--- src/main/java/com/gamegoo/controller/board/BoardController.java
+++ src/main/java/com/gamegoo/controller/board/BoardController.java
@@ -6,6 +6,8 @@ import com.gamegoo.dto.board.BoardRequest;
 import com.gamegoo.dto.board.BoardResponse;
 import com.gamegoo.service.board.BoardService;
 import com.gamegoo.util.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/posts")
+@Tag(name = "Board", description = "게시판 관련 API")
 public class BoardController {
     private final BoardService boardService;
 
     @PostMapping("")
+    @Operation(summary = "게시판 글 작성 API", description = "게시판에서 글을 작성하는 API 입니다.")
     public ApiResponse<BoardResponse.boardInsertResponseDTO> boardInsert(
             @RequestBody BoardRequest.boardInsertDTO request
             ){
