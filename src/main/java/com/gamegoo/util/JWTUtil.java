e46d80845abfb9a57cd5f25983c87de04fbd44d7
Rimi <lora3226@inu.ac.kr>
2024-07-23 17:44:35 +0900

    :sparkles: [feat] github action 수정

--- src/main/java/com/gamegoo/util/JWTUtil.java
+++ src/main/java/com/gamegoo/util/JWTUtil.java
@@ -17,7 +17,7 @@ public class JWTUtil {
 
     private final SecretKey secretKey;
 
-    public JWTUtil(@Value("${JWT_SECRET}") String secret) {
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
