8fb5ff4ed1cc3d07d3a2d351c9dfc34479a84f57
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-27 00:55:10 +0900

    :sparkles: [Feat] api url /v1/으로 변경 및 cors 설정 추가

--- src/main/java/com/gamegoo/controller/member/ProfileController.java
+++ src/main/java/com/gamegoo/controller/member/ProfileController.java
@@ -9,28 +9,36 @@ import com.gamegoo.dto.member.MemberResponse;
 import com.gamegoo.service.member.ProfileService;
 import com.gamegoo.util.JWTUtil;
 import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/v1/member")
 @Slf4j
 public class ProfileController {
+
     private final ProfileService profileService;
 
     @PutMapping("/gamestyle")
     @Operation(summary = "gamestyle 추가 및 수정 API 입니다.", description = "API for Gamestyle addition and modification ")
-    public ApiResponse<List<MemberResponse.GameStyleResponseDTO>> addGameStyle(@RequestBody MemberRequest.GameStyleRequestDTO gameStyleRequestDTO) {
+    public ApiResponse<List<MemberResponse.GameStyleResponseDTO>> addGameStyle(
+        @RequestBody MemberRequest.GameStyleRequestDTO gameStyleRequestDTO) {
         Long memberId = JWTUtil.getCurrentUserId();
-        List<MemberGameStyle> memberGameStyles = profileService.addMemberGameStyles(gameStyleRequestDTO, memberId);
+        List<MemberGameStyle> memberGameStyles = profileService.addMemberGameStyles(
+            gameStyleRequestDTO, memberId);
 
-        List<MemberResponse.GameStyleResponseDTO> dtoList = memberGameStyles.stream().map(memberGameStyle -> MemberResponse.GameStyleResponseDTO.builder()
+        List<MemberResponse.GameStyleResponseDTO> dtoList = memberGameStyles.stream()
+            .map(memberGameStyle -> MemberResponse.GameStyleResponseDTO.builder()
                 .gameStyleId(memberGameStyle.getGameStyle().getId())
                 .gameStyleName(memberGameStyle.getGameStyle().getStyleName())
                 .build()).collect(Collectors.toList());
@@ -40,7 +48,8 @@ public class ProfileController {
 
     @PutMapping("/position")
     @Operation(summary = "주/부 포지션 수정 API 입니다.", description = "API for Main/Sub Position Modification")
-    public ApiResponse<String> modifyPosition(@RequestBody @Valid MemberRequest.PositionRequestDTO positionRequestDTO) {
+    public ApiResponse<String> modifyPosition(
+        @RequestBody @Valid MemberRequest.PositionRequestDTO positionRequestDTO) {
         Long userId = JWTUtil.getCurrentUserId();
         int mainP = positionRequestDTO.getMainP();
         int subP = positionRequestDTO.getSubP();
@@ -52,7 +61,8 @@ public class ProfileController {
 
     @PutMapping("/profile_image")
     @Operation(summary = "프로필 이미지 수정 API 입니다.", description = "API for Profile Image Modification")
-    public ApiResponse<String> modifyPosition(@RequestBody MemberRequest.ProfileImageRequestDTO profileImageDTO) {
+    public ApiResponse<String> modifyPosition(
+        @RequestBody MemberRequest.ProfileImageRequestDTO profileImageDTO) {
         Long userId = JWTUtil.getCurrentUserId();
         String profileImage = profileImageDTO.getProfileImage();
 
