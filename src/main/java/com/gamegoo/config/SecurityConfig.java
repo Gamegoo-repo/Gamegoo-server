a248ffc1a9e02b04f26147f48dd88d7e329fa078
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-26 18:50:24 +0900

    :bug: [Fix] 시큐리티 테스트용 API 추가

--- src/main/java/com/gamegoo/config/SecurityConfig.java
+++ src/main/java/com/gamegoo/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.gamegoo.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import com.gamegoo.apiPayload.exception.handler.JWTExceptionHandlerFilter;
 import com.gamegoo.filter.JWTFilter;
 import com.gamegoo.filter.LoginFilter;
 import com.gamegoo.repository.member.MemberRepository;
 import com.gamegoo.security.CustomUserDetailService;
 import com.gamegoo.util.JWTUtil;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,28 +26,27 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
     private final CustomUserDetailService customUserDetailService;
     private final MemberRepository memberRepository;
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+        throws Exception {
         return configuration.getAuthenticationManager();
     }
 
     @Bean
     public JWTFilter jwtFilter() {
-        List<String> excludedPaths = Arrays.asList("/swagger-ui/", "/v3/api-docs", "/api/member/join", "/api/member/login", "/api/member/email", "/api/member/refresh", "/api/member/riot");
+        List<String> excludedPaths = Arrays.asList("/swagger-ui/", "/v3/api-docs",
+            "/api/member/join", "/api/member/login", "/api/member/email", "/api/member/refresh",
+            "/api/member/riot", "/api/security/permit");
         return new JWTFilter(jwtUtil, excludedPaths, customUserDetailService);
 
     }
@@ -57,20 +60,24 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
 
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .cors(withDefaults())
-                .authorizeHttpRequests((auth) -> auth
-                        .antMatchers("/", "/api/member/join", "/api/member/login", "/api/member/email/**", "/api/member/refresh", "/api/member/riot").permitAll()
-
-                        .antMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated())
-                .addFilterBefore(new JWTExceptionHandlerFilter(), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil, memberRepository), UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(jwtFilter(), LoginFilter.class)
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .cors(withDefaults())
+            .authorizeHttpRequests((auth) -> auth
+                .antMatchers("/", "/api/member/join", "/api/member/login", "/api/member/email/**",
+                    "/api/member/refresh", "/api/member/riot").permitAll()
+                .antMatchers("/api/security/permit").permitAll()
+                .antMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated())
+            .addFilterBefore(new JWTExceptionHandlerFilter(),
+                UsernamePasswordAuthenticationFilter.class)
+            .addFilterAt(
+                new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil,
+                    memberRepository), UsernamePasswordAuthenticationFilter.class)
+            .addFilterBefore(jwtFilter(), LoginFilter.class)
+            .sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
