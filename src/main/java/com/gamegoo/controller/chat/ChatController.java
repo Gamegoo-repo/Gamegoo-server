49f43e79e10485fdc80e39d3d5f55cc1b5d34f20
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-28 04:24:02 +0900

    :sparkles: [Feat] 채팅방 생성 테스트용 API 구현

--- src/main/java/com/gamegoo/controller/chat/ChatController.java
+++ src/main/java/com/gamegoo/controller/chat/ChatController.java
@@ -1,14 +1,22 @@
 package com.gamegoo.controller.chat;
 
 import com.gamegoo.apiPayload.ApiResponse;
+import com.gamegoo.converter.ChatConverter;
+import com.gamegoo.domain.chat.Chatroom;
+import com.gamegoo.dto.chat.ChatRequest;
+import com.gamegoo.dto.chat.ChatResponse;
+import com.gamegoo.service.chat.ChatCommandService;
 import com.gamegoo.service.chat.ChatQueryService;
 import com.gamegoo.util.JWTUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +28,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChatController {
 
     private final ChatQueryService chatQueryService;
+    private final ChatCommandService chatCommandService;
 
     @Operation(summary = "채팅방 uuid 조회 API", description = "회원이 속한 채팅방의 uuid를 조회하는 API 입니다.")
     @GetMapping("/member/chatroom/uuid")
     public ApiResponse<List<String>> getChatroomUuid() {
-        Long memberId = JWTUtil.getCurrentUserId(); //헤더에 있는 jwt 토큰에서 id를 가져오는 코드
+        Long memberId = JWTUtil.getCurrentUserId();
         List<String> chatroomUuids = chatQueryService.getChatroomUuids(memberId);
         return ApiResponse.onSuccess(chatroomUuids);
+    }
+
+    @Operation(summary = "채팅방 생성 API", description = "채팅방을 생성하는 API 입니다.")
+    @PostMapping("/chatroom/create/test")
+    public ApiResponse<ChatResponse.ChatroomCreateResultDto> createChatroom(
+        @RequestBody @Valid ChatRequest.ChatroomCreateRequest request
+    ) {
+        Long memberId = JWTUtil.getCurrentUserId();
+        Chatroom chatroom = chatCommandService.createChatroom(request, memberId);
+        return ApiResponse.onSuccess(
+            ChatConverter.toChatroomCreateResultDto(chatroom, request.getTargetMemberId()));
     }
 
 }
---
+++ src/main/java/com/gamegoo/converter/ChatConverter.java
@@ -0,0 +1,20 @@
+package com.gamegoo.converter;
+
+import com.gamegoo.domain.chat.Chatroom;
+import com.gamegoo.dto.chat.ChatResponse;
+
+public class ChatConverter {
+
+    public static ChatResponse.ChatroomCreateResultDto toChatroomCreateResultDto(Chatroom chatroom,
+        Long targetMemberId) {
+
+        return ChatResponse.ChatroomCreateResultDto.builder()
+            .chatroomId(chatroom.getId())
+            .chatroomType(chatroom.getChatroomType())
+            .uuid(chatroom.getUuid())
+            .postUrl(chatroom.getPostUrl())
+            .targetMemberId(targetMemberId)
+            .build();
+    }
+
+}
