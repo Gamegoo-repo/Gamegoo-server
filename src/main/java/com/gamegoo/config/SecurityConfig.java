417652b307f6b58eae2c51d51c7d25f6a8f872fb
hzee97 <janghyeonji0830@naver.com>
2024-07-28 15:42:31 +0900

    :bug: [Fix] 게시판 글 목록 조회 비회원 접근 가능하도록 변경

--- src/main/java/com/gamegoo/config/SecurityConfig.java
+++ src/main/java/com/gamegoo/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     public JWTFilter jwtFilter() {
         List<String> excludedPaths = Arrays.asList("/swagger-ui/", "/v3/api-docs",
             "/v1/member/join", "/v1/member/login", "/v1/member/email", "/v1/member/refresh",
-            "/v1/member/riot");
+            "/v1/member/riot","/v1/posts");
         return new JWTFilter(jwtUtil, excludedPaths, customUserDetailService);
 
     }
@@ -66,7 +66,7 @@ public class SecurityConfig {
             .cors(withDefaults())
             .authorizeHttpRequests((auth) -> auth
                 .antMatchers("/", "/v1/member/join", "/v1/member/login", "/v1/member/email/**",
-                    "/v1/member/refresh", "/v1/member/riot").permitAll()
+                    "/v1/member/refresh", "/v1/member/riot","/v1/posts").permitAll()
                 .antMatchers("/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(new JWTExceptionHandlerFilter(),
