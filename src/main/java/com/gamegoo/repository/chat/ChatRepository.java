c1fd7ed27b69033016efc820bb921cce48daaf76
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-30 03:03:28 +0900

    :sparkles: [Feat] 채팅 메시지 읽음 처리 API 구현

--- src/main/java/com/gamegoo/repository/chat/ChatRepository.java
+++ src/main/java/com/gamegoo/repository/chat/ChatRepository.java
@@ -1,6 +1,7 @@
 package com.gamegoo.repository.chat;
 
 import com.gamegoo.domain.chat.Chat;
+import com.gamegoo.domain.chat.Chatroom;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,6 @@ public interface ChatRepository extends JpaRepository<Chat, Long>, ChatRepositor
 
     Optional<Chat> findFirstByChatroomIdOrderByCreatedAtDesc(Long chatroomId);
 
+    Optional<Chat> findByChatroomAndTimestamp(Chatroom chatroom, Long timestamp);
 
 }
