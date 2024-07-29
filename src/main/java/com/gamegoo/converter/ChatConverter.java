b2f2e50ebbf35713a47ac1495d9f221db047704b
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-30 02:52:32 +0900

    :sparkles: [Feat] 채팅 메시지 내역 조회 API 구현

--- src/main/java/com/gamegoo/converter/ChatConverter.java
+++ src/main/java/com/gamegoo/converter/ChatConverter.java
@@ -4,6 +4,9 @@ import com.gamegoo.domain.chat.Chat;
 import com.gamegoo.domain.chat.Chatroom;
 import com.gamegoo.dto.chat.ChatResponse;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.data.domain.Slice;
 
 public class ChatConverter {
 
@@ -30,6 +33,34 @@ public class ChatConverter {
             .createdAt(createdAtIoString)
             .timestamp(chat.getTimestamp())
             .build();
+    }
+
+    public static ChatResponse.ChatMessageListDTO toChatMessageListDTO(Slice<Chat> chat) {
+        List<ChatResponse.ChatMessageDTO> chatMessageDtoList = chat.stream()
+            .map(ChatConverter::toChatMessageDto).collect(Collectors.toList());
+
+        return ChatResponse.ChatMessageListDTO.builder()
+            .chatMessageDtoList(chatMessageDtoList)
+            .list_size(chatMessageDtoList.size())
+            .has_next(chat.hasNext())
+            .next_cursor(chat.hasNext() ? chat.getContent().get(0).getTimestamp()
+                : null) // next cursor를 현재 chat list의 가장 오래된 chat의 timestamp로 주기
+            .build();
+    }
+
+    public static ChatResponse.ChatMessageDTO toChatMessageDto(Chat chat) {
+        // ISO 8601 형식의 문자열로 변환
+        String createdAtIoString = chat.getCreatedAt().format(DateTimeFormatter.ISO_DATE_TIME);
+
+        return ChatResponse.ChatMessageDTO.builder()
+            .senderId(chat.getFromMember().getId())
+            .senderName(chat.getFromMember().getGameName())
+            .senderProfileImg(chat.getFromMember().getProfileImage())
+            .message(chat.getContents())
+            .createdAt(createdAtIoString)
+            .timestamp(chat.getTimestamp())
+            .build();
+
     }
 
 }
