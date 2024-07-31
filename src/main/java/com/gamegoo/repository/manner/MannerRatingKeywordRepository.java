b11eb57284f3dff7c694dac17c57170436f1d508
hzee97 <janghyeonji0830@naver.com>
2024-08-01 01:31:44 +0900

    :sparkles: [Feat] 매너 평가 등록 API 구현

---
+++ src/main/java/com/gamegoo/repository/manner/MannerRatingKeywordRepository.java
@@ -0,0 +1,7 @@
+package com.gamegoo.repository.manner;
+
+import com.gamegoo.domain.manner.MannerRatingKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MannerRatingKeywordRepository extends JpaRepository<MannerRatingKeyword, Long> {
+}
---
+++ src/main/java/com/gamegoo/repository/manner/MannerRatingRepository.java
@@ -0,0 +1,7 @@
+package com.gamegoo.repository.manner;
+
+import com.gamegoo.domain.manner.MannerRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MannerRatingRepository extends JpaRepository<MannerRating, Long> {
+}
