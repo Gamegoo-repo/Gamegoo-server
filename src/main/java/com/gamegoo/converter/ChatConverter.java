fc8719df8b6ccaadbb5df3c20702eaa68b731b37
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-02 21:14:25 +0900

    :recycle: [Refactor] DatetimeUtil 추가 및 서비스 코드 리팩토링

--- src/main/java/com/gamegoo/converter/ChatConverter.java
+++ src/main/java/com/gamegoo/converter/ChatConverter.java
@@ -3,7 +3,7 @@ package com.gamegoo.converter;
 import com.gamegoo.domain.chat.Chat;
 import com.gamegoo.domain.chat.Chatroom;
 import com.gamegoo.dto.chat.ChatResponse;
-import java.time.format.DateTimeFormatter;
+import com.gamegoo.util.DatetimeUtil;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.data.domain.Slice;
@@ -21,15 +21,13 @@ public class ChatConverter {
     }
 
     public static ChatResponse.ChatCreateResultDTO toChatCreateResultDTO(Chat chat) {
-        // ISO 8601 형식의 문자열로 변환
-        String createdAtIoString = chat.getCreatedAt().format(DateTimeFormatter.ISO_DATE_TIME);
 
         return ChatResponse.ChatCreateResultDTO.builder()
             .senderId(chat.getFromMember().getId())
             .senderProfileImg(chat.getFromMember().getProfileImage())
             .senderName(chat.getFromMember().getGameName())
             .message(chat.getContents())
-            .createdAt(createdAtIoString)
+            .createdAt(DatetimeUtil.toKSTString(chat.getCreatedAt()))
             .timestamp(chat.getTimestamp())
             .build();
     }
@@ -48,15 +46,13 @@ public class ChatConverter {
     }
 
     public static ChatResponse.ChatMessageDTO toChatMessageDto(Chat chat) {
-        // ISO 8601 형식의 문자열로 변환
-        String createdAtIoString = chat.getCreatedAt().format(DateTimeFormatter.ISO_DATE_TIME);
 
         return ChatResponse.ChatMessageDTO.builder()
             .senderId(chat.getFromMember().getId())
             .senderName(chat.getFromMember().getGameName())
             .senderProfileImg(chat.getFromMember().getProfileImage())
             .message(chat.getContents())
-            .createdAt(createdAtIoString)
+            .createdAt(DatetimeUtil.toKSTString(chat.getCreatedAt()))
             .timestamp(chat.getTimestamp())
             .build();
 
