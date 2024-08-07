2af687f4d7bd3b3efcfc228ff552be9c6405e46b
hzee97 <janghyeonji0830@naver.com>
2024-08-08 03:27:34 +0900

    :lipstick: [Style] 주석 수정

--- src/main/java/com/gamegoo/controller/board/BoardController.java
+++ src/main/java/com/gamegoo/controller/board/BoardController.java
@@ -118,7 +118,7 @@ public class BoardController {
                                                                            @RequestParam(required = false) Integer mainPosition,
                                                                            @RequestParam(required = false) Boolean voice){
 
-        // <포지션 정보> 전체 : 0 , 빠른대전 : 1, 솔로랭크 : 2, 자유랭크 : 3, 칼바람 나락 : 4
+        // <포지션 정보> 전체: 0, 탑: 1, 정글: 2, 미드: 3, 바텀:4, 서포터:5
         if (mainPosition != null && mainPosition == 0) {
             // 전체 포지션 선택 시 필터링에서 제외
             mainPosition = null;
