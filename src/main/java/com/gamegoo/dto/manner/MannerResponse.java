941f779526316543491adb285a0bcd9dde6e2d44
hzee97 <janghyeonji0830@naver.com>
2024-08-10 03:11:20 +0900

    :sparkles: [Feat] 비매너평가 조회 API 구현

--- src/main/java/com/gamegoo/dto/manner/MannerResponse.java
+++ src/main/java/com/gamegoo/dto/manner/MannerResponse.java
@@ -33,4 +33,13 @@ public class MannerResponse {
     public static class mannerKeywordResponseDTO{
         List<Long> mannerRatingKeywordList;
     }
+
+    @Getter
+    @Builder
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class badMannerKeywordResponseDTO{
+        List<Long> mannerRatingKeywordList;
+    }
 }
