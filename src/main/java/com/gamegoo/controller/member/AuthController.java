8a03c9f9aed16800b8582f5eff6679599f368ddb
Rimi <lora3226@inu.ac.kr>
2024-08-09 22:42:06 +0900

    :sparkles: [feat] 중복확인 없는 이메일 인증 API 추가

--- src/main/java/com/gamegoo/controller/member/AuthController.java
+++ src/main/java/com/gamegoo/controller/member/AuthController.java
@@ -40,12 +40,21 @@ public class AuthController {
         return ApiResponse.onSuccess(MemberConverter.toMyProfileDTO(member));
     }
 
+    @PostMapping("/email/send/join")
+    @Operation(summary = "회원가입용 이메일 인증코드 전송 API 입니다. 중복확인 포함", description = "API for sending email for join")
+    public ApiResponse<String> sendEmailwithCheckDuplication(
+            @RequestBody MemberRequest.EmailRequestDTO emailRequestDTO) {
+        String email = emailRequestDTO.getEmail();
+        authService.sendEmail(email, true);
+        return ApiResponse.onSuccess("인증 이메일을 발송했습니다.");
+    }
+
     @PostMapping("/email/send")
-    @Operation(summary = "이메일 인증코드 전송 API 입니다.", description = "API for sending email")
+    @Operation(summary = "이메일 인증코드 전송 API 입니다. 중복확인 X", description = "API for sending email")
     public ApiResponse<String> sendEmail(
             @RequestBody MemberRequest.EmailRequestDTO emailRequestDTO) {
         String email = emailRequestDTO.getEmail();
-        authService.sendEmail(email);
+        authService.sendEmail(email, false);
         return ApiResponse.onSuccess("인증 이메일을 발송했습니다.");
     }
 
