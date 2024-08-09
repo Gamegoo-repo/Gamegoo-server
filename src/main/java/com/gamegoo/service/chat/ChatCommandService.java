e75717f998bd36c52821514ab339da5ed08f38ed
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-09 19:11:28 +0900

    :sparkles: [Feat] 채팅방 입장,시작 API 응답에 상대와의 친구 여부 값 포함

--- src/main/java/com/gamegoo/service/chat/ChatCommandService.java
+++ src/main/java/com/gamegoo/service/chat/ChatCommandService.java
@@ -14,6 +14,7 @@ import com.gamegoo.repository.chat.ChatRepository;
 import com.gamegoo.repository.chat.ChatroomRepository;
 import com.gamegoo.repository.chat.MemberChatroomRepository;
 import com.gamegoo.repository.member.MemberRepository;
+import com.gamegoo.service.member.FriendService;
 import com.gamegoo.service.member.ProfileService;
 import com.gamegoo.util.MemberUtils;
 import java.time.LocalDateTime;
@@ -30,6 +31,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatCommandService {
 
     private final ProfileService profileService;
+    private final FriendService friendService;
     private final MemberRepository memberRepository;
     private final MemberChatroomRepository memberChatroomRepository;
     private final ChatroomRepository chatroomRepository;
@@ -88,6 +90,7 @@ public class ChatCommandService {
                 .memberId(targetMember.getId())
                 .gameName(targetMember.getGameName())
                 .memberProfileImg(targetMember.getProfileImage())
+                .friend(friendService.isFriend(member, targetMember))
                 .blocked(MemberUtils.isBlocked(targetMember, member))
                 .chatMessageList(chatMessageListDTO)
                 .build();
@@ -131,6 +134,7 @@ public class ChatCommandService {
                 .memberId(targetMember.getId())
                 .gameName(targetMember.getGameName())
                 .memberProfileImg(targetMember.getProfileImage())
+                .friend(friendService.isFriend(member, targetMember))
                 .blocked(false)
                 .chatMessageList(null)
                 .build();
@@ -279,6 +283,7 @@ public class ChatCommandService {
             .memberId(targetMember.getId())
             .gameName(targetMember.getGameName())
             .memberProfileImg(targetMember.getProfileImage())
+            .friend(friendService.isFriend(member, targetMember))
             .blocked(MemberUtils.isBlocked(targetMember, member))
             .chatMessageList(chatMessageListDTO)
             .build();
