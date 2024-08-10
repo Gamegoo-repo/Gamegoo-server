e91d665932c96c305b317d89a117003064703928
hzee97 <janghyeonji0830@naver.com>
2024-08-11 07:33:20 +0900

    :sparkles: [Feat] 매너점수 산정 및 매너레벨 결정, 업데이트

--- src/main/java/com/gamegoo/repository/manner/MannerRatingRepository.java
+++ src/main/java/com/gamegoo/repository/manner/MannerRatingRepository.java
@@ -2,9 +2,13 @@ package com.gamegoo.repository.manner;
 
 import com.gamegoo.domain.manner.MannerRating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MannerRatingRepository extends JpaRepository<MannerRating, Long> {
+    List<MannerRating> findByToMemberId(Long toMember);
     List<MannerRating> findByFromMemberIdAndToMemberId(Long fromMember, Long toMember);
+
 }
