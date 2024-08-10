e8e5fc60bde31d65ea5e099a8d14b346721c86d3
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-11 02:34:09 +0900

    :recycle: [Refactor] 알림 생성 메소드 리팩토링

--- src/main/java/com/gamegoo/service/notification/NotificationService.java
+++ src/main/java/com/gamegoo/service/notification/NotificationService.java
@@ -47,34 +47,39 @@ public class NotificationService {
                 notificationTypeTitle)
             .orElseThrow(() -> new NotificationHandler(ErrorStatus.NOTIFICATION_TYPE_NOT_FOUND));
 
-        switch (notificationTypeTitle) {
-            case FRIEND_REQUEST_SEND:
-                return createFriendRequestSendNotification(notificationType, content, member);
-            case FRIEND_REQUEST_RECEIVED:
+        String notificationContent = switch (notificationTypeTitle) {
+            case FRIEND_REQUEST_SEND, FRIEND_REQUEST_ACCEPTED, FRIEND_REQUEST_REJECTED ->
+                content + notificationType.getContent();
+
+            case FRIEND_REQUEST_RECEIVED -> {
                 if (sourceId == null) {
                     throw new NotificationHandler(ErrorStatus.NOTIFICATION_METHOD_BAD_REQUEST);
                 }
-                return createFriendRequestReceivedNotification(notificationType, content, sourceId,
-                    member);
-            case FRIEND_REQUEST_ACCEPTED:
-            case FRIEND_REQUEST_REJECTED:
-                return createFriendRequestAcceptedAndRejectedNotification(notificationType, content,
-                    member);
-            case MANNER_LEVEL_UP:
-            case MANNER_LEVEL_DOWN:
-                return createMannerLevelUpDownNotification(notificationType, content, member);
-            case MANNER_KEYWORD_RATED:
-                return createMannerKeywordRatedNotification(notificationType, content, member);
-            case TEST_ALARM:
+                yield content + notificationType.getContent();
+            }
+
+            case MANNER_LEVEL_UP, MANNER_LEVEL_DOWN, MANNER_KEYWORD_RATED ->
+                notificationType.getContent().replace("n", content);
+
+            case TEST_ALARM -> {
                 // 랜덤 숫자 생성
                 Random random = new Random();
                 int i = random.nextInt(1000) + 1;
-                // 숫자를 String 형으로 변환
-                String randomNumberString = Integer.toString(i);
-                return createTestNotification(notificationType, randomNumberString, member);
-            default:
-                throw new NotificationHandler(ErrorStatus.NOTIFICATION_TYPE_NOT_FOUND);
-        }
+                yield notificationType.getContent() + i;
+            }
+
+            default -> throw new NotificationHandler(ErrorStatus.NOTIFICATION_TYPE_NOT_FOUND);
+        };
+
+        Notification notification = Notification.builder()
+            .notificationType(notificationType)
+            .content(notificationContent)
+            .sourceId(sourceId)
+            .isRead(false)
+            .build();
+        notification.setMember(member);
+
+        return notificationRepository.save(notification);
     }
 
     /**
@@ -155,134 +160,4 @@ public class NotificationService {
             .filter(notification -> !notification.isRead())
             .count();
     }
-
-    /**
-     * 친구 요청 전송됨 알림 생성
-     *
-     * @param notificationType
-     * @param content
-     * @param member
-     * @return
-     */
-    private Notification createFriendRequestSendNotification(
-        NotificationType notificationType,
-        String content, Member member) {
-        Notification notification = Notification.builder()
-            .notificationType(notificationType)
-            .content(content + notificationType.getContent())
-            .isRead(false)
-            .build();
-        notification.setMember(member);
-
-        return notificationRepository.save(notification);
-    }
-
-    /**
-     * 친구 요청 받음 알림 생성
-     *
-     * @param notificationType
-     * @param content
-     * @param sourceId
-     * @param member
-     * @return
-     */
-    private Notification createFriendRequestReceivedNotification(
-        NotificationType notificationType,
-        String content, Long sourceId, Member member
-    ) {
-        Notification notification = Notification.builder()
-            .notificationType(notificationType)
-            .content(content + notificationType.getContent())
-            .sourceId(sourceId)
-            .isRead(false)
-            .build();
-        notification.setMember(member);
-
-        return notificationRepository.save(notification);
-    }
-
-    /**
-     * 친구 요청 수락됨, 거절됨 알림 생성
-     *
-     * @param notificationType
-     * @param content
-     * @param member
-     * @return
-     */
-    private Notification createFriendRequestAcceptedAndRejectedNotification(
-        NotificationType notificationType,
-        String content, Member member
-    ) {
-        Notification notification = Notification.builder()
-            .notificationType(notificationType)
-            .content(content + notificationType.getContent())
-            .isRead(false)
-            .build();
-        notification.setMember(member);
-
-        return notificationRepository.save(notification);
-    }
-
-    /**
-     * 매너레벨 상승, 하락 알림 생성
-     *
-     * @param notificationType
-     * @param content
-     * @param member
-     * @return
-     */
-    private Notification createMannerLevelUpDownNotification(NotificationType notificationType,
-        String content, Member member) {
-        String notificationContent = notificationType.getContent().replace("n", content);
-        Notification notification = Notification.builder()
-            .notificationType(notificationType)
-            .content(notificationContent)
-            .isRead(false)
-            .build();
-        notification.setMember(member);
-
-        return notificationRepository.save(notification);
-    }
-
-    /**
-     * 키워드 평가 알림 생성
-     *
-     * @param notificationType
-     * @param content
-     * @param member
-     * @return
-     */
-    private Notification createMannerKeywordRatedNotification(NotificationType notificationType,
-        String content, Member member) {
-        String notificationContent = notificationType.getContent().replace("n", content);
-        Notification notification = Notification.builder()
-            .notificationType(notificationType)
-            .content(notificationContent)
-            .isRead(false)
-            .build();
-        notification.setMember(member);
-
-        return notificationRepository.save(notification);
-    }
-
-    /**
-     * 테스트 알림 생성
-     *
-     * @param notificationType
-     * @param content
-     * @param member
-     * @return
-     */
-    private Notification createTestNotification(NotificationType notificationType, String content,
-        Member member) {
-        Notification notification = Notification.builder()
-            .notificationType(notificationType)
-            .content(notificationType.getContent() + content)
-            .isRead(false)
-            .build();
-        notification.setMember(member);
-
-        return notificationRepository.save(notification);
-    }
-
 }
