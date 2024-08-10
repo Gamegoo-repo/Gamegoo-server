4f68fcbd8472ed44a23d8fd9c6817a059979c6c3
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-11 01:02:19 +0900

    :sparkles: [Feat] 알림 팝업 목록 조회 API 구현

---
+++ src/main/java/com/gamegoo/controller/notification/NotificationController.java
@@ -0,0 +1,51 @@
+package com.gamegoo.controller.notification;
+
+import com.gamegoo.apiPayload.ApiResponse;
+import com.gamegoo.converter.NotificationConverter;
+import com.gamegoo.domain.notification.Notification;
+import com.gamegoo.dto.notification.NotificationResponse;
+import com.gamegoo.service.notification.NotificationService;
+import com.gamegoo.util.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/v1/notification")
+@Tag(name = "Notification", description = "Notification 관련 API")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @Operation(summary = "알림 목록 조회 API", description = "알림 팝업 화면에서 알림 목록을 조회하는 API 입니다.")
+    @Parameters({
+        @Parameter(name = "cursor", description = "페이징을 위한 커서, Long 타입 notificationId를 보내주세요. 보내지 않으면 가장 최근 알림 10개를 조회합니다."),
+        @Parameter(name = "type", description = "알림 타입 조회 필터, general 또는 friend를 입력해주세요.")
+    })
+    @GetMapping
+    public ApiResponse<NotificationResponse.cursorNotificationListDTO> getNotificationList(
+        @RequestParam(name = "type") String type,
+        @RequestParam(name = "cursor", required = false) Long cursor
+    ) {
+
+        Long memberId = JWTUtil.getCurrentUserId();
+
+        Slice<Notification> notifications = notificationService.getNotificationListByCursor(
+            memberId, type, cursor);
+
+        return ApiResponse.onSuccess(
+            NotificationConverter.toCursorNotificationListDTO(notifications));
+    }
+
+
+}
