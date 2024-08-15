4086224613e8895585e30abdbdfd86d5a61487c2
Rimi <lora3226@inu.ac.kr>
2024-08-15 09:39:15 +0900

    :sparkles: [feat] 이미 매챙에 있는 사람 입장에서 우선순위 계산하기 & Response DTO

--- src/main/java/com/gamegoo/dto/matching/MatchingResponse.java
+++ src/main/java/com/gamegoo/dto/matching/MatchingResponse.java
@@ -1,10 +1,19 @@
 package com.gamegoo.dto.matching;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 public class MatchingResponse {
-    public static class PriorityMatchingResponse {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PriorityMatchingResponseDTO {
         List<MemberPriority> myPriorityList;
         List<MemberPriority> otherPriorityList;
     }
