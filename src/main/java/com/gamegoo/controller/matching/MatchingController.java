a19fff1009bdc208e0f79f9829312ec5797ca03e
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-08-20 02:01:12 +0900

    :sparkles: [Feat] 나와 상대의 매칭 상태 변경 API 구현

--- src/main/java/com/gamegoo/controller/matching/MatchingController.java
+++ src/main/java/com/gamegoo/controller/matching/MatchingController.java
@@ -25,11 +25,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/matching")
 @Slf4j
 public class MatchingController {
+
     private final MatchingService matchingService;
 
     @PostMapping("/priority")
     @Operation(summary = "우선순위 계산 및 매칭 기록을 저장하는 API 입니다.", description = "API for calculating and recording matching")
-    public ApiResponse<MatchingResponse.PriorityMatchingResponseDTO> saveMatching(@RequestBody @Valid MatchingRequest.InitializingMatchingRequestDTO request) {
+    public ApiResponse<MatchingResponse.PriorityMatchingResponseDTO> saveMatching(
+        @RequestBody @Valid MatchingRequest.InitializingMatchingRequestDTO request) {
         Long id = JWTUtil.getCurrentUserId();
 
         // 매칭 타입 유효성 검사
@@ -40,7 +42,8 @@ public class MatchingController {
         }
 
         // 우선순위 계산
-        MatchingResponse.PriorityMatchingResponseDTO priorityMatchingResponseDTO = matchingService.getPriorityLists(request, id);
+        MatchingResponse.PriorityMatchingResponseDTO priorityMatchingResponseDTO = matchingService.getPriorityLists(
+            request, id);
 
         // DB에 기록하기
         matchingService.save(request, id);
@@ -55,5 +58,18 @@ public class MatchingController {
 
         matchingService.updateMyStatus(request, id);
         return ApiResponse.onSuccess("매칭 상태 변경에 성공했습니다.");
+    }
+
+    @PatchMapping("/status/target/{targetMemberId}")
+    @Parameter(name = "targetMemberId", description = "매칭 기록을 수정할 상대 회원의 id 입니다.")
+    @Operation(summary = "나와 상대 회원의 매칭 기록 상태 수정 API", description = "나와 특정 상대 회원의 매칭 기록 상태를 수정하는 API 입니다.")
+    public ApiResponse<String> modifyBothMatching(
+        @RequestBody @Valid MatchingRequest.ModifyMatchingRequestDTO request,
+        @PathVariable(name = "targetMemberId") Long targetMemberId) {
+        Long memberId = JWTUtil.getCurrentUserId();
+
+        matchingService.updateBothStatus(request, memberId, targetMemberId);
+
+        return ApiResponse.onSuccess("매칭 상태 변경 성공");
     }
 }
