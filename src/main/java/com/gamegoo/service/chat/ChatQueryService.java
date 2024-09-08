2a49c9b18e52ea318c36aad53a449a3886d54bfd
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-09-08 23:15:36 +0900

    :recycle: [Refactor] 채팅방 목록 조회 API의 gameName 탈퇴 여부 고려

--- src/main/java/com/gamegoo/service/chat/ChatQueryService.java
+++ src/main/java/com/gamegoo/service/chat/ChatQueryService.java
@@ -80,7 +80,8 @@ public class ChatQueryService {
                     .uuid(chatroom.getUuid())
                     .targetMemberId(targetMember.getId())
                     .targetMemberImg(targetMember.getProfileImage())
-                    .targetMemberName(targetMember.getGameName())
+                    .targetMemberName(
+                        targetMember.getBlind() ? "(탈퇴한 사용자)" : targetMember.getGameName())
                     .friend(friendService.isFriend(member, targetMember))
                     .blocked(MemberUtils.isBlocked(targetMember, member))
                     .blind(targetMember.getBlind())
