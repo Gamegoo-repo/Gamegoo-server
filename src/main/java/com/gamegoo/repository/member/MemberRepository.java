56d4492eb4e5129f73ee0421a4652eca02c3d3f8
Rimi <lora3226@inu.ac.kr>
2024-09-19 15:25:34 +0900

    :sparkles: [feat] 이미 있는 사용자인지 확인할 때 탈퇴한 경우, 탈퇴한 사용자라는 error 보내기

--- src/main/java/com/gamegoo/repository/member/MemberRepository.java
+++ src/main/java/com/gamegoo/repository/member/MemberRepository.java
@@ -13,8 +13,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
-    Optional<Member> findByGameName(String gameName);
-
     Optional<Member> findById(Long id);
 
     Optional<Member> findByRefreshToken(String refresh_token);
