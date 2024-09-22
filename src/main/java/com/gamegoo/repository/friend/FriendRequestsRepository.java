9f4df9f4e968aa8188287a9b2fd591e6611e4249
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-09-23 07:51:35 +0900

    :sparkles: [Feat] 회원 탈퇴 API에 채팅, 친구 관련 로직 추가

--- src/main/java/com/gamegoo/repository/friend/FriendRequestsRepository.java
+++ src/main/java/com/gamegoo/repository/friend/FriendRequestsRepository.java
@@ -3,6 +3,7 @@ package com.gamegoo.repository.friend;
 import com.gamegoo.domain.friend.FriendRequestStatus;
 import com.gamegoo.domain.friend.FriendRequests;
 import com.gamegoo.domain.member.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,9 @@ public interface FriendRequestsRepository extends JpaRepository<FriendRequests, 
     Optional<FriendRequests> findByFromMemberAndToMemberAndStatus(Member fromMember,
         Member toMember, FriendRequestStatus status);
 
+    List<FriendRequests> findAllByFromMemberAndStatus(Member fromMember,
+        FriendRequestStatus status);
+
+    List<FriendRequests> findAllByToMemberAndStatus(Member toMember,
+        FriendRequestStatus status);
 }
